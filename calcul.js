6f824e1eb22338a30a99bacf7707944fdc87fdeb
Régis Broage <rbroage@users.noreply.github.com>
2023-11-22 17:31:14 +0100

    Create calcul.js

---
+++ calcul.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+// Fonction pour calculer la somme et le produit de deux nombres.
+function calculate(n1, n2, operation) {
+  if (operation === "add") {
+    result = add(n1, n2);
+  } else if (operation === "multiply") {
+    result = multiply(n1, n2);
+  } else {
+    throw new Error("Invalid operation");
+  }
+  return result;
+}
