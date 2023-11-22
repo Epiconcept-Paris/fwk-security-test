28cde93113b07b0009b3d8d86be2333a915501eb
Régis Broage <r.broage@epiconcept.fr>
2023-11-22 17:35:48 +0100

    incorrect calc

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
+import calculate from './calcul.js';
+
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const iResult = calculate('un mot', 'un autre mot', 'multiply');
+
+    return 'Hello World! ' + iResult;
   }
 }
--- src/calcul.js
+++ src/calcul.js
@@ -7,13 +7,13 @@ function multiply(a, b) {
 }
 
 // Fonction pour calculer la somme et le produit de deux nombres.
-function calculate(n1, n2, operation) {
-  if (operation === "add") {
+export default calculate = (n1, n2, operation) => {
+  if (operation === 'add') {
     result = add(n1, n2);
-  } else if (operation === "multiply") {
+  } else if (operation === 'multiply') {
     result = multiply(n1, n2);
   } else {
-    throw new Error("Invalid operation");
+    throw new Error('Invalid operation');
   }
   return result;
-}
+};
