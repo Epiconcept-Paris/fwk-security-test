80276acf1ceef6460dc272c5fc2adc928acdcff0
Régis Broage <r.broage@epiconcept.fr>
2023-11-22 17:10:03 +0100

    add secrets

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,31 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        auth: {
+          username: configService.get<string>('MONGODB_USERNAME'),
+          password: configService.get<string>('MONGODB_PASSWORD'),
+        },
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        auth: {
+          username: configService.get<string>('MONGODB_USERNAME_2'),
+          password: 'password12345',
+        },
+        uri: configService.get<string>('MONGODB_URI_2'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
 export class AuthModule {}
